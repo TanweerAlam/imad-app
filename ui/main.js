66b651f4b5c57b99c5088fedd15dd2608f65ef8e
Md Tanweer Alam <tanweeralam1312@gmail.com>
2018-02-19 23:39:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,5 +11,5 @@ button.onclick = function(){
     counter = counter + 1;
     var span = document.getElementById('count');
     span.innerHTML = counter.toString();
-    console.log(counter);
+    alert(counter);
 };
