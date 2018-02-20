c4e0d7f593e04601fcaab68447201bcbea99f303
Md Tanweer Alam <tanweeralam1312@gmail.com>
2018-02-21 00:54:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -30,13 +30,30 @@ var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
-    // Make a request to the server and send the name
-    //Capture a list of name and render in a list of name
-    var names = ['name1', 'name2', 'name3', 'name4'];
-    var list = '';
-    for(var i = 0; i < names.length; i++){
-        list += '<li>' + names[i] + '</li>';
-    }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
+    
+    //Create a request object
+    var request = new XMLHttpRequest();
+    
+    //Capture the request and store it in a variable
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttp.DONE){
+            //Take some action
+            if(request.status === 200){
+             
+                //Capture a list of name and render in a list of name
+                var names = request.responseText;
+                names = JSON.parse(names);
+                var list = '';
+                for(var i = 0; i < names.length; i++){
+                    list += '<li>' + names[i] + '</li>';
+                }
+                var ul = document.getElementById('namelist');
+                ul.innerHTML = list;
+            }
+        }
+        //Not done yet
+    };
+    //Make the request
+    request.open('GET', 'http://tanweeralam1312.imad.hasura-app.io/submit-name?name' + name, true);
+    request.send(null);
 };
