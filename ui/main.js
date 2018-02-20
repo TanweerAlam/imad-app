1c171b7eafdeda8e54c00bfff27945d347b3359d
Md Tanweer Alam <tanweeralam1312@gmail.com>
2018-02-21 01:32:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -53,6 +53,6 @@ submit.onclick = function(){
     //Make the request
     var nameInput = document.getElementById('name');
     var name = nameInput.value;
-    request.open('GET', 'http://tanweeralam1312.imad.hasura-app.io/submit-name?name' + name, true);
+    request.open('GET', 'http://tanweeralam1312.imad.hasura-app.io/submit-name?name=' + name, true);
     request.send(null);
 };
