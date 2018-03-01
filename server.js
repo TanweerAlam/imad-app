663b60a9d18a4e09a8446634103d98eedbdd7ab9
Md Tanweer Alam <tanweeralam1312@gmail.com>
2018-03-01 18:04:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
+var Pool = require('pg').pool;
 var app = express();
+
+var config = {
+    user: 'tanweeralam1312',
+    database: 'tanweeralam1312',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+};
 app.use(morgan('combined'));
 
 var articles = {
@@ -80,6 +88,20 @@ function createTemplate(data){
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+var pool = new Pool(config);
+app.get('/test-db', function (req, res){
+    //make a select request
+    pool.query('SELECT * FROM test', function(err, result){
+        if(err){
+            res.status(500).send(err.toString());
+        }
+        else{
+            res.send(JSON.stringify(result));
+        }
+    });
+    //return a response with the results
 });
 
 var counter = 0;
